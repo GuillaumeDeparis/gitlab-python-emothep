c09a37ba06b6abbcb00996441d75f40e8b2cbda6
GuillaumeDeparis <deparis.guillaume@gmail.com>
2022-04-14 15:22:40 +0200

    Ajout des config pour Centos

--- configfile - Centos.py
+++ configfile - Centos.py
@@ -3,9 +3,10 @@ GITLAB_URL = 'http://10.2.39.18/'
 GITLAB_TOKEN = 'PHFqULc91MVG7jZnZy2g'
 NAMESPACE = 'emothep/run/is-run'
 PATH_REPO_PACKAGES = '/assets/IS/Packages/'
-TEMPLATEREPO = 'C:\git\gitlab-python-emothep\webmethods-assets-is-template.zip'
-LOCALREPO = 'C:\SoftwareAG107\localPackages'
+TEMPLATEREPO = './webmethods-assets-is-template.zip'
+LOCALREPO = '/opt/localPackages'
 #[Softwareag]
-SAGHOME='C:\SoftwareAG107\IntegrationServer\instances\default\packages'
+SAGHOME='/opt/softwareag/IntegrationServer/instances/default/packages'
+
 #https://github.com/ansible-collections/community.general/blob/main/plugins/module_utils/gitlab.py
 #https://github.com/ansible-collections/community.general/blob/7e6a2453d0786052b1640c5c602e6ac152c75947/plugins/modules/source_control/gitlab/gitlab_project.py#L259
